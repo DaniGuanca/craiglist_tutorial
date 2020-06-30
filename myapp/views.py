73c87e0af8bc8af03c34afc582fea8d7ac8dcbca
Dani <dany12rp13@gmail.com>
2020-06-30 16:47:39 -0300

    Agrego archivos frontend

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,17 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home (request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return render(request,'')
+    search = request.POST.get('Buscar')
+    
+    #en esta variable guardo todo lo que mandan del frontend
+    cosas_del_frontend = {
+        'buscar' : search,
+    }
+
+    return render(request,'myapp/new_search.html', cosas_del_frontend)
